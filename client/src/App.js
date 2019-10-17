5dd42708337cc6a03f0f2f8a76d71caa38bad5d8
Steve Renner <che1111@hotmail.com>
2019-10-16 20:28:53 -0700

    Added Form

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { Route } from "react-router-dom";
 import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
 import Movie from "./Movies/Movie";
+import MovieForm from "./Movies/MovieForm";
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
@@ -16,9 +17,9 @@ const App = () => {
       <SavedList list={savedList} />
       <Route exact path="/" component={MovieList} />
       <Route
-        path="/movies/:id"
+        path="/update-movie/:id"
         render={props => {
-          return <Movie {...props} addToSavedList={addToSavedList} />;
+          return <MovieForm {...props} />
         }}
       />
     </>
---
+++ client/src/Movies/MovieForm.js
@@ -0,0 +1,80 @@
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+
+const initialMovie = {
+    title: '',
+    director: '',
+    metascore: '',
+    starts: []
+}
+
+const MovieForm = (props) => {
+    const [movieToUpdate, setMovieToUpdate] = useState(initialMovie)
+
+    useEffect(() => {
+        if (props.location.state !== undefined)
+        setMovieToUpdate(props.location.state)
+    }, [props.location.state])
+
+    const changeHandler = event => {
+        event.persist()
+        let value = event.target.value
+            if (event.target.name === 'stars') {
+                value = value.split(',')
+            }
+        setMovieToUpdate({ ...movieToUpdate, [event.target.name]: value })
+    }
+
+    const handleSubmit = event => {
+        event.preventDefault()
+            if (props.location.state !== undefined) {
+                axios
+                .put('http://localhost5000/api/movies/$(movieToUpDate/id', movieToUpdate)
+                .then(res => {
+                    setMovieToUpdate(initialMovie)
+                    props.history.push('/')
+                })
+                .catch(err => console.log(err))
+            }
+    }
+    
+    return (
+        <div>
+            <legend>Update Movie</legend>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type='text'
+                    name='title'
+                    onChange={changeHandler}
+                    placeholder='Title'
+                    value={movieToUpdate.title}
+                />
+                <input
+                    type='text'
+                    name='director'
+                    onChange={changeHandler}
+                    placeholder='Director'
+                    value={movieToUpdate.director}
+                />
+                <input
+                    type='number'
+                    name='metascore'
+                    onChange={changeHandler}
+                    placeholder="Metascore"
+                    value={movieToUpdate.metascore}
+                />
+                <input
+                    type='text'
+                    name='stars'
+                    onChange={changeHandler}
+                    placeholder='Stars'
+                    value={movieToUpdate.starts.toString()}
+                />
+
+                <button type='submit'>{props.location.state} !== undefined ? 'Update' : 'Add'</button>
+            </form>
+        </div>
+    )
+}
+
+export default MovieForm;
